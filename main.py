25627d365bc0626e78eef545dff233a4f78fdff1
alsaidi33 <32727363+alsaidi33@users.noreply.github.com>
2025-08-11 11:16:57 +0400

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,7 @@ def _cors(r: Response):
     return r
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Render will bind to the port from $PORT
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
